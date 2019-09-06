2815f51d001d663f05b832541617527a8f938197
Anthony Moutte <instabledesign@gmail.com>
2019-09-06 16:32:36 +0200

    implement tag print:hide

--- config/base.go
+++ config/base.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"time"
+
+	"github.com/gol4ng/skeleton/pkg/config"
 )
 
 type Base struct {
@@ -9,12 +11,16 @@ type Base struct {
 	DocumentIndexName string `config:"document_index_name"`
 
 	ElasticURL            string        `config:"elastic_url"`
-	ElasticUsername       string        `config:"elastic_username"`
-	ElasticPassword       string        `config:"elastic_password"`
+	ElasticUsername       string        `config:"elastic_username" print:"-"`
+	ElasticPassword       string        `config:"elastic_password" print:"-"`
 	ElasticIndexPrefixURL string        `config:"elastic_index_prefix"`
 	ElasticTimeout        time.Duration `config:"elastic_timeout"`
 	ElasticMaxIdleConns   int           `config:"elastic_max_idle_connections"`
 	ElasticMaxRetry       int           `config:"elastic_max_retry"`
 
 	Server
+}
+
+func (c *Base) String() string {
+	return config.ToString(c)
 }
